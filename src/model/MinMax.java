e3ed3c62d118eaa0db92b3c2d3811833ce319ab0
Paulo Ricardo <paulorism@gmail.com>
2016-05-10 17:28:49 -0300

    minmix melhoria

--- src/model/MinMax.java
+++ src/model/MinMax.java
@@ -3,59 +3,58 @@ package model;
 import java.util.List;
 
 public class MinMax {
-    
-    private int profundidadeAtual;
-    private int profundidadeMaxima = 10;
 
+	private int profundidadeAtual;
+	private int profundidadeMaxima = 10;
 
-    public Tabuleiro buscaMelhorJogada(Tabuleiro tabuleiroAtual) {
-        profundidadeAtual = 0;
-        
-        List<Jogada> jogadas = tabuleiroAtual.getJogadasPossiveis();
-        
-        Jogada melhorJogada = null;
-        
-        int melhorScore = Integer.MIN_VALUE;
-        
-        
-        for(Jogada jogada : jogadas){
-            int score = partiuProximo(jogada, tabuleiroAtual, true);
-            if(score > melhorScore){
-                melhorScore = score;
-                melhorJogada = jogada;
-            }
-        }
-        
-        return melhorJogada;
-    }
-    
-    private int partiuProximo(Jogada jogada, Tabuleiro tabuleiroAtual, boolean max) {
-        Tabuleiro novoTabuleiro = tabuleiroAtual.jogar(jogada);
-        
-        if(novoTabuleiro.ehFinal() 
-                || profundidadeAtual == profundidadeMaxima){
-            return novoTabuleiro.getValor();
-        } else {
-            List<Jogada> jogadas = novoTabuleiro.getJogadasPossiveis();
-        
-            int melhorValor;
-            if(max)
-            	melhorValor = Integer.MIN_VALUE;
-            else 
-            	melhorValor = Integer.MAX_VALUE;
-            
-            for(Jogada jogadaFor : jogadas){
-                int valor = partiuProximo(jogadaFor, novoTabuleiro, !max);
-                
-                if((max && valor > melhorValor) 
-                        || (!max && valor < melhorValor)){
-                    melhorValor = valor;
-                }
-            }
-            
-            
-            return melhorValor;
-        }
-    }*/
-    
+	public Tabuleiro buscaMelhorJogada(Tabuleiro tabuleiroAtual) {
+		profundidadeAtual = 0;
+
+		List<Tabuleiro> jogadas = geraJogadasPossiveis();
+
+		Tabuleiro melhorJogada = null;
+
+		int melhorScore = Integer.MIN_VALUE;
+
+		for (Tabuleiro jogada : jogadas) {
+			int score = partiuProximo(jogada, true);
+			if (score > melhorScore) {
+				melhorScore = score;
+				melhorJogada = jogada;
+			}
+		}
+
+		return melhorJogada;
+	}
+
+	private List<Tabuleiro> geraJogadasPossiveis() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private int partiuProximo(Tabuleiro jogada, boolean max) {
+
+		if (jogada.ehFinal() || profundidadeAtual == profundidadeMaxima) {
+			return jogada.getValor();
+		} else {
+			List<Tabuleiro> jogadas = geraJogadasPossiveis();
+
+			int melhorValor;
+			if (max)
+				melhorValor = Integer.MIN_VALUE;
+			else
+				melhorValor = Integer.MAX_VALUE;
+
+			for (Tabuleiro proxTabuleiro : jogadas) {
+				int valor = partiuProximo(proxTabuleiro, !max);
+
+				if ((max && valor > melhorValor) || (!max && valor < melhorValor)) {
+					melhorValor = valor;
+				}
+			}
+
+			return melhorValor;
+		}
+	}
+
 }
