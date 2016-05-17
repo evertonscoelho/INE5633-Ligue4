526cf06cfe7d4c089947cc41ef78d8f3a397ef70
Paulo Ricardo <paulorism@gmail.com>
2016-05-17 15:56:50 -0300

    Constantes criadas

---
+++ src/model/Constantes.java
@@ -0,0 +1,15 @@
+package model;
+
+public final class Constantes {
+
+	public static final int LINHAS = 6;
+	public static final int COLUNAS = 7;
+	public static final int VALOR_DUPLA = 2;
+	public static final int VALOR_TRIPLA = 20;
+	public static final int VALOR_QUADRA = 1000;
+	public static final int VALOR_CASA_VAZIA = 1000;
+	public static final int PROFUNDIDADE_MAXIMA = 5;
+	
+}
+
+
